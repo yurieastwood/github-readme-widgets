1fc6f2a29ebfae7c92f8eade9d23d88168c8befe
Yuri Eastwood <yurieastwood@users.noreply.github.com>
2023-12-10 16:39:46 -0600

    Update error-card.js
    Fixing line break

--- src/cards/error-card.js
+++ src/cards/error-card.js
@@ -6,7 +6,7 @@ const renderError = (message, secondaryMessage="") => {
                 .small { font: 600 12px 'Segoe UI', Ubuntu, Sans-Serif; fill: #252525 }
                 .gray { fill: #858585 }
             </style>
-            <text x="25" y="45" class="text">Something went wrong! file an issue at <br> https://github.com/mechdeveloper/credly-readme-widget</text>
+            <text x="25" y="45" class="text">Something went wrong! file an issue at </br> https://github.com/mechdeveloper/credly-readme-widget</text>
             <br>
             <text data-testid="message" x="25" y="55" class="text small">
                 <tspan x="25" dy="18">${message}</tspan>
